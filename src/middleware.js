136c7ce0b2d8c30e301fbe9275e409a3a6693ec5
hieuvovan <hieuvo0904@gmail.com>
2023-03-03 16:04:50 +0700

    debug

--- src/middleware.js
+++ src/middleware.js
@@ -4,13 +4,15 @@ import { NextResponse } from 'next/server';
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
   const referer = request.headers.get('referer');
+  const userAgent = request.headers.get('user-agent');
   const pathname = request?.nextUrl?.pathname || '';
   const destinationHost = process.env.WORDPRESS_GRAPHQL_ENDPOINT.split('/graphql')[0]
   const slug = pathname.split('/posts')[1]
 
-  console.log(`${destinationHost}${slug}`)
-  console.log(referer)
-  console.log(request.headers)
+  console.log('destinationHost', `${destinationHost}${slug}`)
+  console.log('referer', referer)
+  console.log('request.headers', request.headers)
+  console.log('userAgent', userAgent)
 
   if (referer?.includes('facebook.com')) return NextResponse.rewrite(`${destinationHost}${slug}`);
 }
